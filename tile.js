8936e1b6c1b34dda835502a86233f5399ede2ba6
roslyyvik <roslyyvik@gmail.com>
2019-08-13 18:32:28 +0300

    new js

--- tile.js
+++ tile.js
@@ -1,13 +1,20 @@
 $("#btn").click(function(e) {
-    $("#myContainer").slideDown("slow");
+    $(".search").fadeIn();
+    $("#myText").addClass(".active").focus;
+    $(".mystyle").css({ "display": "none" });
     e.stopPropagation();
 });
 $("#myContainer").click(function(e) {
     e.stopPropagation();
 });
-$(document).click(function(e) {
-    $("#myContainer").slideUp("slow");
+$("input").focus(function() {
+    $(".mystyle").slideDown("slow");
 });
+$(document).click(function(e) {
+    $(".mystyle").slideUp();
+    $(".search").fadeOut("slow");
+});
+
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
